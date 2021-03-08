41b54fd1699275a86d36c88bec8693c94c5177ac
Lenar <flfforwork@gmail.com>
2021-03-08 12:44:53 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let openBracket = [];
+let closeBracket = [];
+let copyStr = str;
+let index = 0;
+let openItem = '';
+for (let brack of bracketsConfig) {
+openBracket.push(brack[0]);
+closeBracket.push(brack[1]);
+}
+
+for (let item of str) {
+if (closeBracket.includes(item)) {
+index = closeBracket.indexOf(item);
+openItem = openBracket[index];
+copyStr = copyStr.replace((openItem + item), "");
+ }
+}
+return copyStr === ""
 }
